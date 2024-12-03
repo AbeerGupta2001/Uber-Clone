99f6852b5f0c715408ad5fd788363e958aff2148
AbeerGupta2001 <abeergupta2001@gmail.com>
2024-12-03 12:52:36 +0530

    captain model completed

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,28 +1,48 @@
 import User from "../models/user.model.js";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import BlackListToken from "../models/blacklistToken.model.js";
+import Captain from "../models/captain.model.js";
 
+export const authUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-const authUser = async(req,res,next)=>{
-    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
-    if(!token){
-        return res.status(401).json({message:"Unauthorized"});
-    }
+  const isBlackListed = await BlackListToken.findOne({ token: token });
+  if (isBlackListed) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    const isBlackListed = await BlackListToken.findOne({token:token})
-    if(isBlackListed){
-        return res.status(401).json({message: "Unauthorized"})
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded._id);
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
 
-    try {
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        const user = await User.findById(decoded._id);
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(401).json({message:"Unauthorized"})
-    }
-}
+export const authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
-export default authUser
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const isBlackListed = await BlackListToken.findOne({ token: token });
+  if (isBlackListed) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const captain = await Captain.findById(decoded._id);
+    req.captain = captain;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 import { body } from "express-validator";
-import { registerCaptain } from "../controllers/captain.controller.js";
+import { getCaptainProfile, loginCaptain, logoutCaptain, registerCaptain } from "../controllers/captain.controller.js";
+import { authCaptain } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -30,5 +31,15 @@ router.post(
   ],
   registerCaptain
 );
+
+router.post('/login',[
+    body('email').isEmail().withMessage("Invalid Email"),
+    body('password').isLength({min:3}).withMessage("Password must be at least 3 characters long")
+],loginCaptain)
+
+router.get('/profile',authCaptain,getCaptainProfile);
+
+router.get('/logout',authCaptain,logoutCaptain)
+
 
 export default router;
