7c8a108ff172f621e06f173e513f836ec4ad333f
AbeerGupta2001 <abeergupta2001@gmail.com>
2024-12-03 10:47:14 +0530

    captain created

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
+import captainRoutes from './routes/captain.routes.js'
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(cookieParser())
 
 app.use("/users", userRoutes);
+app.use("/captains",captainRoutes)
 
 app.get("/", (req, res) => {
   res.send("Hi Abeer");
